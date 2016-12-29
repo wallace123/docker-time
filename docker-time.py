a7cfccf20f2b935e23264c52457319658a74fa89
wallace123 <bwtibbs@hotmail.com>
2016-12-29 08:57:49 -0500

    Adding optparse logic

--- docker-time.py
+++ docker-time.py
@@ -1,3 +1,20 @@
 """ Searches docker binaries and edits the timestamp """
+from optparse import OptionParser
+
+def main():
+    parser = OptionParser(usage="usage %prog [options] filename")
+
+    parser.add_option("-f", "--file",
+                      action="store_true",
+                      dest="docker_file",
+                      default=False,
+                      help="read in the docker binary file for timestamp modification")
+
+    (options, args) = parser.parse_args()
+
+    if len(args) != 1:
+        parser.error("wrong number of arguments")
 
 
+if __name__ == '__main__':
+    main()
