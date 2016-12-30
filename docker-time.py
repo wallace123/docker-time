191985d17c41bd8a56433c97725d0ef8021eee3c
wallace123 <bwtibbs@hotmail.com>
2016-12-30 06:10:08 -0500

    Modifying timestamp of dockerfile

--- docker-time.py
+++ docker-time.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import argparse
+import re
 
 
 ONE_GB = 1024*1024*1024
@@ -17,17 +18,45 @@ def readFile(infile):
         f.close()
 
     return f_chars
-        
+
+
+def subTimeStamp(instr):
+    # Timestamp example: 2016-12-16T02:42:17.070078439+00:00
+    regExpr = r'2016-\d\d-\d\dT\d\d:\d\d:\d\d\.\d\d\d\d\d\d\d\d\d\+\d\d:\d\d'
+    searchObj = re.search(regExpr, 
+                          instr,
+                          re.M|re.I)
+
+    if searchObj:
+        print "timestamp found: ", searchObj.group()
+    else:
+        print "Timestamp not found"
+        sys.exit(0)
+
+    outstr = re.sub(regExpr,
+                    r'2016-01-01T00:00:00.000000000+00:00',
+                    instr)
+
+    return outstr
+
+
+def writeFile(outstr, outfile):
+    with open(outfile, 'w') as f:
+        f.write(outstr)
+        f.close()
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('infile',
                         help="Input file to search for timestamp")
+    parser.add_argument('outfile',
+                        help="Output file with modified timestamp")
     args = parser.parse_args()
 
-    f_chars = readFile(args.infile)
-    for i in range(10):
-        print f_chars[i]
+    in_chars = readFile(args.infile)
+    out_chars = subTimeStamp(in_chars)    
+    writeFile(out_chars, args.outfile)
 
 
 if __name__ == '__main__':
